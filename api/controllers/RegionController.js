c062025ad5a757ab004dca5deb82d4b822bf875e
Shripad Joshi <shripad.joshi.s@gmail.com>
2016-07-15 15:01:41 +0530

    Region added

---
+++ api/controllers/RegionController.js
@@ -0,0 +1,11 @@
+/**
+ * RegionController
+ *
+ * @description :: Server-side logic for managing Regions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
