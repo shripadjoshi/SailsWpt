8430b34153ab107a349d31474ec2f400fb714b9c
Shripad Joshi <shripad.joshi.s@gmail.com>
2016-07-15 15:48:33 +0530

    Browser added

---
+++ api/controllers/BrowserController.js
@@ -0,0 +1,11 @@
+/**
+ * BrowserController
+ *
+ * @description :: Server-side logic for managing Browsers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
