e0b7eba8bc2ec6f66f3f31812a5b8683eab281ce
Shripad Joshi <shripad.joshi.s@gmail.com>
2016-07-19 15:35:18 +0530

    Property added

---
+++ api/controllers/PropertyController.js
@@ -0,0 +1,11 @@
+/**
+ * PropertyController
+ *
+ * @description :: Server-side logic for managing Properties
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
