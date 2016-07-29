085d08f55990173e08f2ef16315c5d7a653f2a67
Shripad Joshi <shripad.joshi.s@gmail.com>
2016-07-29 17:48:28 +0530

    Location search functionality changes

--- api/services/LocationService.js
+++ api/services/LocationService.js
@@ -1,17 +1,28 @@
 module.exports = {
 
     searchLocation: function(field, q, next) {
-        var locQuery = Location.find();
-        if(field == 'Active' || field == 'In-Active'){
-        	locQuery.where({ 'active': (field == 'Active' ? true : false) });
-        }
-        else {
-        	console.log(field);
-        	locQuery.where({ ("'"+field+"'"): { startsWith: q } });	
+        var locQuery = Location.find().populate(['location_region','location_browser']);
+
+        if (field == 'Active' || field == 'In-Active') {
+            locQuery.where({ 'active': (field == 'Active' ? true : false) });
+        } else {
+            console.log(field);
+            if (field == 'name') {
+                locQuery.where({
+                    'name': { startsWith: q }
+                });
+            } else if (field == 'display_name') {
+                locQuery.where({
+                    'display_name': { startsWith: q }
+                });
+            }else if(field == 'location_region'){
+                locQuery.where(populate('location_region', { name: q }));
+            }
+
         }
 
         locQuery.exec(function(err, locations) {
-            if(err) throw err;
+            if (err) throw err;
             next(locations);
         });
     }
