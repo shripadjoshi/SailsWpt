3cff3d01274f290489aefb31bd17bf701fab9fc6
Shripad Joshi <shripad.joshi.s@gmail.com>
2016-08-01 13:02:25 +0530

    location search changes

--- api/services/LocationService.js
+++ api/services/LocationService.js
@@ -1,13 +1,14 @@
 module.exports = {
 
     searchLocation: function(field, q, next) {
-        var locQuery = Location.find().populate(['location_region','location_browser']);
+        var locQuery = Location.find().populate(['location_region', 'location_browser']);
 
+        //console.log(field);
         if (field == 'Active' || field == 'In-Active') {
             locQuery.where({ 'active': (field == 'Active' ? true : false) });
         } else {
-            console.log(field);
-            if (field == 'name') {
+            if(field == ''){}
+            else if (field == 'name') {
                 locQuery.where({
                     'name': { startsWith: q }
                 });
@@ -15,16 +16,58 @@ module.exports = {
                 locQuery.where({
                     'display_name': { startsWith: q }
                 });
-            }else if(field == 'location_region'){
-                locQuery.where(populate('location_region', { name: q }));
+            } else if (field == 'location_region') {
+                regQuery = Region.find().populate(['locations']);
+                regQuery.where({
+                    'name': { startsWith: q }
+                });
+                var regionIds = []
+                regQuery.exec(function(err, regions) {
+                    if (err) throw err;
+                    for (var i = 0; i < regions.length; i++) {
+                        regionIds.push(regions[i].id)
+                    }
+                    locQuery.where({
+                        'location_region': regionIds
+                    });
+                    locQuery.exec(function(err, locations) {
+                        if (err) throw err;
+                        next(locations);
+                    });
+
+                });
+
+            } else {
+                browserQuery = Browser.find().populate(['locations']);
+                browserQuery.where({
+                    'name': { startsWith: q }
+                });
+                var browserIds = []
+                browserQuery.exec(function(err, browsers) {
+                    if (err) throw err;
+                    for (var i = 0; i < browsers.length; i++) {
+                        browserIds.push(browsers[i].id)
+                    }                    
+                    locQuery.where({
+                        'location_browser': browserIds
+                    });
+                    locQuery.exec(function(err, locations) {
+                        if (err) throw err;
+                        next(locations);
+                    });
+
+                });
+
             }
 
         }
 
-        locQuery.exec(function(err, locations) {
-            if (err) throw err;
-            next(locations);
-        });
+        if (field != 'location_region' && field != 'location_browser') {
+            locQuery.exec(function(err, locations) {
+                if (err) throw err;
+                next(locations);
+            });
+        }
     }
 
 
